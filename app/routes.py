ca39d6923efa094c0e2e7e0f3120c47e1dd87d23
Chrisna <chrisnaadhip@gmail.com>
2020-09-08 12:36:30 +0700

    First Commit
    First!

---
+++ app/routes.py
@@ -0,0 +1,24 @@
+from flask import render_template
+from app import app
+from app.forms import LoginForm
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Chrisna'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Nice move you got there!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'Touhou 3D - Imperishable Night is the most nice things in 2020!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
